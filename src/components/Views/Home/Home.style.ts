5c899ab456ba7e3cbd5541bb94ee4f0aa870602b
Kirsten Lindsmith <kirstenlindsmith@gmail.com>
2022-08-05 14:37:12 -0400

    WIP: eye gaze with mid points

--- src/components/Views/Home/Home.style.ts
+++ src/components/Views/Home/Home.style.ts
@@ -21,43 +21,50 @@ export const LogoSection = styled.div`
 export const LogoEye = styled.div`
   position: absolute;
   z-index: 10;
-  bottom: 25%;
-  left: 32%;
-  width: 15%;
-  height: 27%;
+  bottom: 30%;
+  left: 32.1%;
+  width: 5.6rem;
+  height: 6rem;
   border-radius: 50%;
   background: ${colorValues.offWhite};
 `;
 
-export const LogoEyeLids = styled.div`
+export const LogoEyeLidTop = styled.div`
   position: absolute;
-  z-index: 1;
-  bottom: 24%;
-  left: 32%;
-  width: 15%;
-  height: 29%;
-  border-radius: 50%;
-  background: ${colorValues.blue};
+  z-index: 1000;
+  left: 31.9%;
+  top: 54%;
+  width: 5.75rem;
+  height: 9rem;
+  border-radius: 100%;
+  background-color: transparent;
+  box-shadow: 0px -2.5rem 0px 0px ${colorValues.lightBlue};
+`;
+
+export const LogoEyeLidBottom = styled(LogoEyeLidTop)`
+  top: auto;
+  bottom: 31%;
+  box-shadow: 0px 2.5rem 0px 0px ${colorValues.lightBlue};
 `;
 
 export const LogoEyeTearDucts = styled.div`
   position: absolute;
   z-index: 1;
-  bottom: 26%;
   left: 31%;
+  bottom: 26%;
   width: 17%;
   height: 25%;
   border-radius: 50%;
-  background: ${colorValues.lightBlue};
+  background: ${colorValues.blue};
 `;
 
 export const LogoPupil = styled.div`
   position: absolute;
   z-index: 100;
-  bottom: 33%;
-  left: 27%;
-  height: 40%;
-  width: 64%;
+  left: 1.35rem;
+  top: 1.6rem;
+  height: 3rem;
+  width: 3rem;
   border-radius: 50%;
   background: ${colorValues.black};
 `;
--- src/components/Views/Home/Home.tsx
+++ src/components/Views/Home/Home.tsx
@@ -4,25 +4,96 @@ import {
   Page,
   LogoSection,
   LogoEye,
-  LogoEyeLids,
+  LogoEyeLidTop,
+  LogoEyeLidBottom,
   LogoEyeTearDucts,
   LogoPupil,
 } from './Home.style';
 import logo from '../../../assets/KirstenCodes.png';
 
+const eyeXStart = '1.35rem';
+const eyeYStart = '1.6rem';
+const eyeMinimum = '0';
+const eyeMaximum = 'calc(100% - 3.1rem)';
+const eyeMedTop = 'calc(100% - 5.1rem)';
+const eyeMedBottom = 'calc(100% - 3.7rem);';
+const eyeMedLeft = 'calc(100% - 5rem)';
+const eyeMedRight = 'calc(100% - 3.6rem);';
+
 const Home = () => {
-  const [eyeX, setEyeX] = React.useState(0);
-  const [eyeY, setEyeY] = React.useState(0);
+  const [eyeX, setEyeX] = React.useState(eyeXStart);
+  const [eyeY, setEyeY] = React.useState(eyeYStart);
   // const [eyeRotation, setEyeRotation] = React.useState(0);
 
   const moveEye = (e: MouseEvent) => {
     const eye = document?.querySelector('#eye');
-    const mouseX = eye.getBoundingClientRect().right;
-    const mouseY = eye.getBoundingClientRect().top;
+    const eyeLeftBound = eye.getBoundingClientRect().left;
+    const eyeRightBound = eye.getBoundingClientRect().right;
+    const eyeTopBound = eye.getBoundingClientRect().top;
+    const eyeBottomBound = eye.getBoundingClientRect().bottom;
+    const mouseX = e.pageX;
+    const mouseY = e.pageY;
+    const inYMidLine = mouseY > eyeTopBound && mouseY < eyeBottomBound;
+    const inXMidLine = mouseX > eyeLeftBound && mouseX < eyeRightBound;
+    console.log(
+      // 'eyeLeftBound:',
+      // eyeLeftBound,
+      // 'eyeRightBound:',
+      // eyeRightBound,
+      // '\n',
+      // 'eyeTopBound:',
+      // eyeTopBound,
+      // 'eyeBottomBound:',
+      // eyeBottomBound,
+      // '\n',
+      'mouseX:',
+      mouseX,
+      'mouseY:',
+      mouseY,
+      '\n',
+      'mouseX - eyeRightBound:',
+      mouseX - eyeRightBound,
+      '\n',
+      'eyeTopBound - mouseY:',
+      eyeTopBound - mouseY,
+      '\n',
+      'mouseY - eyeBottomBound:',
+      mouseY - eyeBottomBound
+    );
 
-    // const radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
-    // const rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
-    // setEyeRotation(rotationDegrees);
+    //vertical gaze position
+    if (mouseY < eyeTopBound) {
+      //if mouse is above the eye
+      if (eyeTopBound - mouseY > 100) {
+        setEyeY(eyeMinimum);
+      } else if (eyeTopBound - mouseY > 0) {
+        setEyeY(eyeMedTop);
+      } else setEyeY(eyeYStart);
+    } else if (mouseY > eyeBottomBound) {
+      //if mouse is below the eye
+      if (mouseY - eyeBottomBound > 100) {
+        setEyeY(eyeMaximum);
+      } else if (mouseY - eyeBottomBound > 0) {
+        setEyeY(eyeMedBottom);
+      } else setEyeY(eyeYStart);
+    } else if (inYMidLine) setEyeY(eyeYStart);
+
+    //horizontal gaze position
+    if (mouseX < eyeLeftBound) {
+      //if mouse is left of the eye
+      if (eyeLeftBound - mouseX > 100) {
+        setEyeX(eyeMinimum);
+      } else if (eyeLeftBound - mouseX > 0) {
+        setEyeX(eyeMedLeft);
+      } else setEyeX(eyeXStart);
+    } else if (mouseX > eyeRightBound) {
+      //if mouse is right of the eye
+      if (mouseX - eyeRightBound > 100) {
+        setEyeX(eyeMaximum);
+      } else if (mouseX - eyeRightBound > 0) {
+        setEyeX(eyeMedRight);
+      } else setEyeX(eyeXStart);
+    } else if (inXMidLine) setEyeX(eyeXStart);
   };
 
   React.useEffect(() => {
@@ -36,9 +107,16 @@ const Home = () => {
     <Page>
       <LogoSection>
         <img src={logo} />
-        <LogoEye />
-        <LogoPupil id='eye' />
-        <LogoEyeLids />
+        <LogoEyeLidTop />
+        <LogoEye id='eye'>
+          <LogoPupil
+            style={{
+              left: eyeX,
+              top: eyeY,
+            }}
+          />
+        </LogoEye>
+        <LogoEyeLidBottom />
         <LogoEyeTearDucts />
       </LogoSection>
       <Button>CLICK ME</Button>
